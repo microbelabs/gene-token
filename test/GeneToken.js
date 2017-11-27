87e6afa148d0caa444e4ed22d920c2437379d9f9
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2017-11-27 19:28:35 +0700

    Add basic unit test

---
+++ test/GeneToken.js
@@ -0,0 +1,41 @@
+const GeneToken = artifacts.require("GENEToken")
+
+
+contract("GENEToken", accounts => {
+  const [geneAccount, secondAccount, thirdAccount] = accounts;
+
+  it("should put total supply to owner balance", async () => {
+    try {
+      const token = await GeneToken.deployed();
+      const totalSupply = await token.totalSupply.call();
+      const balance = await token.balanceOf.call(geneAccount);
+      
+      assert.equal(balance.valueOf(), totalSupply.valueOf(), "Account balance not equal to total supply");
+    } catch (e) {
+      throw new Error(e);
+    }
+  });
+
+
+  it("should transfer amount of token from master to secondary account", async () => {
+    try {
+      const token = await GeneToken.deployed();
+      const geneAccountInitialBalance = await token.balanceOf.call(geneAccount);
+      const secondAccountInitialBalance = await token.balanceOf.call(secondAccount);
+  
+      const amount = 10000;
+      await token.transfer(secondAccount, amount);
+
+      const geneAccountFinalBalance = await token.balanceOf.call(geneAccount);
+      const secondAccountFinalBalance = await token.balanceOf.call(secondAccount);
+
+      assert.equal(geneAccountFinalBalance.toNumber(), geneAccountInitialBalance.toNumber() - amount);
+      assert.equal(secondAccountFinalBalance.toNumber(), amount);
+      
+    } catch (e) {
+        throw new Error(e);
+    }
+  });
+
+});
+
