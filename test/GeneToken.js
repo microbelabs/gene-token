99a23ea5280d3bc1d115f32cd1354ea241e67646
Ribhararnus Pracutian <oknoorap@gmail.com>
2017-11-28 08:59:52 +0700

    add: basic gene token unit test

--- test/GeneToken.js
+++ test/GeneToken.js
@@ -1,23 +1,23 @@
-const GeneToken = artifacts.require("GENEToken")
+const GeneToken = artifacts.require('GENEToken')
 
 
-contract("GENEToken", accounts => {
+contract('ERC20 Standard Token Basic Functions', accounts => {
   const [geneAccount, secondAccount, thirdAccount] = accounts;
 
-  it("should put total supply to owner balance", async () => {
+  it('should put total supply to owner balance', async () => {
     try {
       const token = await GeneToken.deployed();
       const totalSupply = await token.totalSupply.call();
       const balance = await token.balanceOf.call(geneAccount);
       
-      assert.equal(balance.valueOf(), totalSupply.valueOf(), "Account balance not equal to total supply");
+      assert.equal(balance.valueOf(), totalSupply.valueOf(), 'Account balance not equal to total supply');
     } catch (e) {
       throw new Error(e);
     }
   });
 
 
-  it("should transfer amount of token from master to secondary account", async () => {
+  it('should transfer amount of token from master to secondary account', async () => {
     try {
       const token = await GeneToken.deployed();
       const geneAccountInitialBalance = await token.balanceOf.call(geneAccount);
@@ -36,6 +36,15 @@ contract("GENEToken", accounts => {
         throw new Error(e);
     }
   });
-
 });
 
+contract('GENE Token Functions', accounts => {
+  it(`should register game`);
+  it('should check registered game already added to list');
+  it('should view complete list of registered game ');
+  it('should add game to trusted list');
+  it('should view list of trusted games');
+  it(`should view game's name`);
+  it(`should view game's name`);
+  it(`should view game's attributes list`);
+});
